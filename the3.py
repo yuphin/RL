0271215a9b63f4794a82edc1e616bb36575aaef6
yunus <yycan77@hotmail.com>
2019-05-24 18:40:18 +0300

    Finalized + adapted to 2.7

--- the3.py
+++ the3.py
@@ -1,10 +1,11 @@
 import numpy as np
-
+#!!! NOTE: This script is only compatible with Python 2.7. Please run as 'python the3.py', not python3
 nodes = []
 learning_rate = discount_factor = None
 T_dt = None
 Q_dt = None
 Q_prob = None
+goal_nodes = []
 actions_prob = []
 states_prob = {}
 transitions_count = None
@@ -29,6 +30,8 @@ def parseInput():
                 nodes.append(idx)
             if n == 'S' or n == 'O':
                 prob_node_count += 1
+            if n == 'G':
+                goal_nodes.append(idx)
 
         learning_rate, discount_factor = map(float, f.readline()[:-1].split())
         num_transitions = int(f.readline()[:-1])
@@ -60,14 +63,14 @@ def parseInput():
             tables = []
             while (True):
                 line = f.readline()[:-1]
-                if line == '#':
+                if line.startswith('#'):
                     break
                 reward = int(line)
                 node_id = int(f.readline()[:-1])
                 table = ActionTable(reward, node_id)
                 while (True):
                     line = f.readline()[:-1]
-                    if line == '$':
+                    if line.startswith('$'):
                         break
                     transition = list(map(int, line.split()))
                     table.transitions[transition[0]] = transition[1]
@@ -92,30 +95,140 @@ def get_action(Q, s, p=True):
     else:
         ind = np.argmax(Q[s])
         return ind
+# Prints the Q values
+def print_Q(Q):
+    m_str = ''
+    for i in range(len(Q)):
+        if i in nodes:
+            continue
+        for j in range(len(Q[i])):
 
+            if Q[i][j] == -np.inf:
+                if j == len(Q[i]) - 1:
+                    m_str += '_'
+                else:
+                    m_str += '_ '
+            else:
+                if j == len(Q[i]) -1:
+                    m_str += str(Q[i][j])
+                else:
+                    m_str += str(Q[i][j]) + ' '
+        m_str += '\n'
+    print m_str
+#For Q-Learning algorithm, not needed for this homework
+def print_p_v(Q):
+    str_v = ''
+    str_p = ''
+    policy = []
+    for idx,row in enumerate(Q):
+        max_v = max(row)
+        max_va = np.where(row == max_v)
+        str_v += str(idx) + ' ' + str(max_v) +'\n'
+        str_p += str(idx) + ' ' + ','.join(max_va[0].astype(str)) + '\n'
+    print(str_v)
+    print(str_p)
 
 # Returns Q values to be printed
-def q_learn(episodes):
-    for end_node in episodes:
-        rate = learning_rate
-        s = 0
-        admission_list = nodes + [end_node]
-        while (True):
-            if s in admission_list:
-                break
-            a = get_action(Q_dt, s)
-            new_action = get_action(Q_dt, a, False)
-            delta = T_dt[s][a] + discount_factor * Q_dt[a][new_action] - Q_dt[s][a]
-            Q_dt[s][a] += rate * delta
-            s = a
-    return Q_dt
+def q_learn():
+    print('Please enter episodes, $ to cancel')
+    episode_tables = []
+    while True:
+        text = raw_input()
+        if text == '$':
+            break
+        else:
+            episodes = list(map(int, text.split()))
+            s = episodes[0]
+            for idx,episode in enumerate(episodes[1:]):
+                rate = learning_rate
+                a = episode
+                if episode in nodes:
+                    nxt = 0
+                else:
+                    nxt = Q_dt[a][episodes[idx+2]]
+                delta = T_dt[s][a] + discount_factor * nxt - Q_dt[s][a]
+                Q_dt[s][a] += rate * delta
+                s = a
+                if episode in nodes:
+                    break
+            episode_tables.append(np.copy(Q_dt))
+            print_Q(Q_dt)
+
+def print_v_p(V,P):
+    for k,v in V.items():
+        print k,v
+    print
+    for k,v in P.items():
+        str_buf = ''
+        if isinstance(v,list):
+            str_buf += str(k) + ' ' + ','.join(map(str, v))
+        else:
+            str_buf += str(k) + ' ' + str(v)
+        print str_buf
+    print
+
+def value_iteration():
+    V = {}
+    P = {}
+    for k, v in states_prob.items():
+        V[k] = 0
+        P[k] = []
+    delta = 0.01
+    print('Starting value iteration...')
+
+    while(True):
+
+        prev = {}
+        for k, v in V.items():
+            prev[k] = v
+        for state, actions in states_prob.items():
+            for action_idx in actions:
+                for at in actions_prob[action_idx]:
+                    if at.node_id == state:
+                        e = []
+                        actions = []
+                        flag = -1
+                        for k, v in at.transitions.items():
+                            if not k in goal_nodes:
+                                e.append(at.reward + discount_factor * float(float(v) / 100) * V[k])
+                                actions.append(k)
+                            else:
+                                flag =k
+                                actions.append(k)
+                        if not e:
+                            e.append(at.reward)
+                        V[state] = max(e)
+                        max_ar = np.where(np.array(e) == V[state])
+                        opt_actions = list(np.take(np.array(actions),max_ar[0]))
+                        P[state] = opt_actions
+
+
+                    else:
+                        continue
+        print_v_p(V,P)
+        print('To continue, enter c, to terminate enter $')
+        text = raw_input()
+        if text == '$':
+            break
+        #For the convergence case, not needed for this homework
+        '''
+        df = 0
+        for k, v in V.items():
+            df += abs(V[k] - prev[k])
+
+        if df < delta:
+           break
+        '''
+    return V
 
 
 if __name__ == "__main__":
-    inp = [4, 5, 6]
-    episodes = []
-    for i in range(5000):
-        episodes.append(np.random.choice(inp))
     parseInput()
-    q_learn(episodes)
-    print(learning_rate)
+    q_learn()
+    value_iteration()
+
+
+
+
+
+
